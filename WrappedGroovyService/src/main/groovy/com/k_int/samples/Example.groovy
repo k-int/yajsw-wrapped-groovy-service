9238c6ea432f7782b7fcdc63b739ce61dd17b5a5
Ian Ibbotson <ianibbo@googlemail.com>
2011-05-23 17:30:06 +0100

    Added initial generation of maven archetype

---
+++ WrappedGroovyService/src/main/groovy/com/k_int/samples/Example.groovy
@@ -0,0 +1,15 @@
+//
+// Generated from archetype; please customize.
+//
+
+package com.k_int.samples
+
+/**
+ * Example Groovy class.
+ */
+class Example
+{
+    def show() {
+        println 'Hello World'
+    }
+}
---
+++ WrappedGroovyService/src/test/groovy/com/k_int/samples/HelperTest.groovy
@@ -0,0 +1,18 @@
+//
+// Generated from archetype; please customize.
+//
+
+package com.k_int.samples
+
+import groovy.util.GroovyTestCase
+
+/**
+ * Tests for the {@link Helper} class.
+ */
+class HelperTest
+    extends GroovyTestCase
+{
+    void testHelp() {
+        new Helper().help(new Example())
+    }
+}
